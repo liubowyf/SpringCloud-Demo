4ec13f6c0d75b36bdd5cbc6011b2aa9445e8e5f6
刘博 <liubowyf@163.com>
2020-07-30 15:05:37 +0800

    添加sentinel使用nacos持久化限流规则配置

--- SpringCloud-Alibaba/gateway-server/src/main/java/com/liubo/gatewayserver/config/RouteLimitConfig.java
+++ SpringCloud-Alibaba/gateway-server/src/main/java/com/liubo/gatewayserver/config/RouteLimitConfig.java
@@ -13,6 +13,13 @@ import reactor.core.publisher.Mono;
 @Configuration
 public class RouteLimitConfig {
 
+    /**
+     * 基于ip的限流
+     * @author liubo
+     * @date 2020/7/30 13:40
+     *  * @param
+     * @return org.springframework.cloud.gateway.filter.ratelimit.KeyResolver
+     */
     @Bean
     public KeyResolver ipKeyResolver() {
         return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
